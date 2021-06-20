1e6be8e31fc1751ce8599c43337801a44b842fc3
Ilya Dukhin <idukhini@mail.ru>
2021-06-21 00:35:40 +0300

    Init commit

---
+++ app/src/main/java/ru/stiznt/mapinkotlin/MainActivity.kt
@@ -0,0 +1,21 @@
+package ru.stiznt.mapinkotlin
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import ovh.plrapps.mapview.MapView
+import ovh.plrapps.mapview.MapViewConfiguration
+import ovh.plrapps.mapview.core.TileStreamProvider
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
+import java.lang.Exception
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
